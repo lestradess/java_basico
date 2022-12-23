4204c24560554cf47dc22c42854bf50d722f0b15
Lestrades <lestrades@gmail.com>
2022-12-23 20:05:34 +0100

    Comentarios, tipos, operadores y  variables

---
+++ src/a_comentarios/tipos_datos/variables/A_HolaMundo.java
@@ -0,0 +1,12 @@
+package a_variables.tiposdatos;
+
+public class A_HolaMundo {
+
+    public static void main(String[] args){
+
+        System.out.println("Hola mundo desde sesión 1 de Open Bootcamp");
+
+    }
+
+
+}
---
+++ src/a_comentarios/tipos_datos/variables/B_comentarios.java
@@ -0,0 +1,26 @@
+package a_variables.tiposdatos;
+
+/**
+ * Clase de prueba para crear un programa básico en java
+ */
+public class B_comentarios {
+
+    //COMENTARIOS JAVA DOC
+    /**
+     * Punto de entrada a la aplicación java
+     * Permite ejecutar la aplicación
+     * @param args - argumentos de entrada para la aplicación
+     */
+    public static void main(String[] args){
+
+        // esto es un comentario
+        System.out.println("Adios mundo cruel!"); // esto es otro comentario
+        /*
+        Esto es un comentario multilínea:
+        1 - Crear funcion suma
+        2 - Crear variables tipo numérico
+        3 - Utilizar la función suma para calcular la suma de los dos números
+         */
+    }
+
+}
